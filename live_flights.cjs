3ad5b96478279b8b9bfad7cb9a2c5552269ccbd8
zcxr-gif <hrantony2230@gmail.com>
2025-09-30 20:14:19 -0400

    update: added front end to debug 2

--- live_flights.cjs
+++ live_flights.cjs
@@ -13,6 +13,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 /* =========================
  * Config
